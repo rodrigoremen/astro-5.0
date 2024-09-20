6a01c136e82aecb05cbdd1d6462abc0835adbedd
Rodrigo Dorantes <rodrigoivan234@gmail.com>
2024-09-20 14:11:34 -0600

    Terminado!

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,22 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, envField } from 'astro/config';
+
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	output: 'server',
+	integrations: [tailwind()],
+	env: {
+		schema: {
+			SHOW_BUY_BOTTON: envField.boolean({
+				context: 'server',
+				access: 'public',
+			}),
+			SCOPE_API_ENDPOINT: envField.string({
+				context: 'server',
+				access: 'public',
+			}),
+		},
+	},
+});
