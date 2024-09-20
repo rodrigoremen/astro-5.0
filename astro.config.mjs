0b8748588f101f0b72a19d1ad5cc45f61a029f01
Rodrigo Dorantes <rodrigoivan234@gmail.com>
2024-09-20 14:20:54 -0600

    Adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,20 +3,25 @@ import { defineConfig, envField } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
+import vercel from '@astrojs/vercel/serverless';
+
 // https://astro.build/config
 export default defineConfig({
-	output: 'server',
-	integrations: [tailwind()],
-	env: {
-		schema: {
-			SHOW_BUY_BOTTON: envField.boolean({
-				context: 'server',
-				access: 'public',
-			}),
-			SCOPE_API_ENDPOINT: envField.string({
-				context: 'server',
-				access: 'public',
-			}),
-		},
+  output: 'server',
+  integrations: [tailwind()],
+
+  env: {
+      schema: {
+          SHOW_BUY_BOTTON: envField.boolean({
+              context: 'server',
+              access: 'public',
+          }),
+          SCOPE_API_ENDPOINT: envField.string({
+              context: 'server',
+              access: 'public',
+          }),
+      },
 	},
+
+  adapter: vercel(),
 });
